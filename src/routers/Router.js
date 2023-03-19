4cc67b94e2c0f10ecd5963c0c9fe288722aa6d7b
Joseph-muigai <wambichojoseph@gmail.com>
2023-03-19 22:19:01 +0300

    initial commit,setup file structure for the project

---
+++ src/routers/Router.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Home from '../pages/Home'
+import Shop from '../pages/Shop'
+import Cart from '../pages/Cart'
+import ProductDetails from '../pages/ProductDetails'
+import Checkout from '../pages/Checkout'
+import Login from '../pages/Login'
+import  Signup from '../pages/Signup'
+import { Route, Routes } from 'react-router-dom'
+const Router = () => {
+  return <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/shop" element={<Shop />} />
+    <Route path="/cart" element={<Cart />} />
+    <Route path="/shop/:id" element={<ProductDetails />} />
+    <Route path="/checkout" element={<Checkout />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/signup" element={<Signup />} />
+    
+  </Routes>
+}
+
+export default Router
