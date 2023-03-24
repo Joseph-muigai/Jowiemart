72c40952f7522115e4f7fcaeb29d5e34b86a4f33
Joseph-muigai <wambichojoseph@gmail.com>
2023-03-24 13:18:58 +0300

    create login page design

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
-
+import Helmet from '../components/Helmet/Helmet'
+import { Container, Row, Col,Form, FormGroup } from 'reactstrap'
+import { motion } from 'framer-motion'
+import { cartActions } from '../redux/cartSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import HeroSection from '../components/Ui/HeroSection'
+import '../styles/login.css'
 const Login = () => {
   return (
-    <div>Login</div>
+    <Helmet title="Login">
+      <HeroSection title="Login" />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" className='m-auto text-center'>
+              <h3 className="fw-bold fs-4">Login</h3>
+
+              <Form className='auth__form'>
+                <FormGroup className='form__group'>
+                   <input type="email" placeholder='Enter your email'/>
+                </FormGroup>
+                <FormGroup className='form__group'>
+                   <input type="password" placeholder='Enter your Password'/>
+                </FormGroup>
+                <button className="buy__btn auth__btn">
+                  Login
+                </button>
+                <p>Dont have an account? <Link to='/signup'>Create an account</Link> </p>
+              </Form>
+
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
   )
 }
 
