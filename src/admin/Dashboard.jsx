953abaa0c795e1e2a8b76e0e53096b2e0022f830
Joseph-muigai <wambichojoseph@gmail.com>
2023-05-26 23:20:46 +0300

    admin dashboard

--- src/admin/Dashboard.jsx
+++ src/admin/Dashboard.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
-
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import "../styles/dashboard.css";
+import useGetData from "../custom_hooks/useGetData";
 const Dashboard = () => {
+  const { data: products } = useGetData("products");
+  const { data: users } = useGetData("users");
   return (
-    <div>Dashboard</div>
-  )
-}
+    <>
+      <section>
+        <Container>
+          <Row>
+            <Col className="lg-3">
+              <div className="revenue__box">
+                <h5>Total Sales</h5>
+                <span>$7970</span>
+              </div>
+            </Col>
+            <Col className="lg-3">
+              <div className="orders__box">
+                <h5>Orders</h5>
+                <span>$797</span>
+              </div>
+            </Col>
+            <Col className="lg-3">
+              <div className="products__box">
+                <h5>Total Products</h5>
+                <span>{products.length}</span>
+              </div>
+            </Col>
+            <Col className="lg-3">
+              <div className="users__box">
+                <h5>Total users</h5>
+                <span>{users.length}</span>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
